1bc9d81ba8b9b1d8872a0b06492b25db5069985e
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-06-21 20:43:00 +0800

    depending on whether the date is selected or not, Add/Edit button active

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,57 +1,93 @@
-import React from "react";
-import {
-  IonContent,
-  IonHeader,
-  IonTitle,
-  IonToolbar,
-  IonDatetime,
-  IonButton,
-} from "@ionic/react";
+import React, { useState } from "react";
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonDatetime, IonButton } from "@ionic/react";
+import { useHistory } from "react-router";
 
-const HomePage = () => (
-  <>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Write your diary</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          height: "100%",
-          paddingBottom: 60,
-          alignItems: "center",
-        }}
-      >
+const HomePage = () => {
+  const history = useHistory();
+  const [selectedDate, setSelectedDate] = useState<string | null>(null); // Track the selected date
+
+  const handleAddClick = () => {
+    if (!selectedDate) return; // If no date selected, do nothing
+
+    // Check if diary exists for selected date
+    const diaryExists = checkDiaryExists(); // Remove the argument
+
+    if (!diaryExists) {
+      history.push("/add"); // Navigate to AddPage if no diary exists
+    }
+  };
+
+  const handleEditClick = () => {
+    if (!selectedDate) return; // If no date selected, do nothing
+
+    // Check if diary exists for selected date
+    const diaryExists = checkDiaryExists(); // Remove the argument
+
+    if (diaryExists) {
+      history.push("/edit"); // Navigate to EditPage if diary exists
+    }
+  };
+
+  const handleDateChange = (event: CustomEvent<any>) => {
+    setSelectedDate(event.detail.value);
+  };
+
+  const checkDiaryExists = () => {
+    // Replace this with your actual logic to check if a diary exists for the given date
+    // Return true if diary exists, false otherwise
+    return false;
+  };
+
+
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Write your diary</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
         <div
           style={{
-            alignSelf: "center",
-            paddingTop: 30,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "100%",
+            paddingBottom: 60,
+            alignItems: "center",
           }}
         >
-          <IonDatetime></IonDatetime>
-        </div>
+          <div
+            style={{
+              alignSelf: "center",
+              paddingTop: 30,
+            }}
+          >
+            <IonDatetime onIonChange={handleDateChange}></IonDatetime>
+          </div>
 
-        <div
-          style={{
-            marginBottom: "5px",
-            width: "100%",
-            maxWidth: "370px",
-          }}
-        >
-          <div style={{ marginTop: "10px" }}>
-            <IonButton expand="block">Add</IonButton>
-          </div>
-          <div style={{ marginTop: "10px" }}>
-            <IonButton expand="block">Edit</IonButton>
+          <div
+            style={{
+              marginBottom: "5px",
+              width: "100%",
+              maxWidth: "370px",
+            }}
+          >
+            <div style={{ marginTop: "10px" }}>
+              <IonButton expand="block" onClick={handleAddClick}>
+                Add
+              </IonButton>
+            </div>
+            <div style={{ marginTop: "10px" }}>
+              <IonButton expand="block" onClick={handleEditClick}>
+                Edit
+              </IonButton>
+            </div>
           </div>
         </div>
-      </div>
-    </IonContent>
-  </>
-);
+      </IonContent>
+    </>
+  );
+};
 
 export default HomePage;
