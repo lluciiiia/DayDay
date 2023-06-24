514cbe6a57769d1e1b8072b359b88d6b0d074b17
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-06-24 12:40:02 +0800

    EditPage: make the UI the same with AddPage

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -1,7 +1,56 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, useIonToast } from '@ionic/react';
+import { useHistory, useLocation } from 'react-router';
+
+interface LocationState {
+  selectedDate: string;
+}
 
 const EditPage: React.FC = () => {
+  const history = useHistory();
+  const location = useLocation<LocationState>();
+  const [content, setContent] = useState('');
+  const [present] = useIonToast();
+
+  const selectedDate = location.state?.selectedDate;
+  const savedContent = localStorage.getItem(selectedDate) || '';
+
+  useEffect(() => {
+    setContent(savedContent);
+  }, [savedContent]);
+
+  const handleSave = () => {
+    if (content.trim() === '') {
+      presentToast('Please enter your diary content');
+      return;
+    }
+
+    if (selectedDate) {
+      saveData(selectedDate, content);
+    }
+
+    presentToast('Your diary is saved!');
+    setTimeout(() => {
+      history.push('/home');
+    }, 1000);
+  };
+
+  const presentToast = (message: string) => {
+    present({
+      message: message,
+      duration: 1500,
+      position: 'middle',
+    });
+  };
+
+  const saveData = (key: string, value: string) => {
+    localStorage.setItem(key, value);
+  };
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,7 +59,44 @@ const EditPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        {/* Add your content here */}
+        <div
+          style={{
+            height: 680,
+            overflowY: 'scroll',
+            maxWidth: 370,
+            margin: '0 auto',
+            padding: '20px 3px',
+          }}
+        >
+          <textarea
+            value={content}
+            onChange={handleContentChange}
+            style={{
+              width: '100%',
+              height: '100%',
+              border: 0,
+              borderRadius: 10,
+              borderColor: 'transparent',
+            }}
+            placeholder="Enter your diary here"
+          ></textarea>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            marginBottom: 10,
+            position: 'fixed',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            padding: '0.5rem',
+          }}
+        >
+          <IonButton fill="outline" id="save" style={{ width: '160px' }} onClick={handleSave}>
+            Save
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
