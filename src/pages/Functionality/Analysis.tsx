1f7204aa693d9b648159da63312e58427fcca2cc
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-02 12:01:59 +0700

    sentiment library changed

--- src/pages/Functionality/Analysis.tsx
+++ src/pages/Functionality/Analysis.tsx
@@ -1,43 +1,42 @@
 import React from "react";
-import afinn from "afinn-111";
+import natural from "natural";
 import graphDot from "./InitializeDot";
 
 // Define an interface for the sentiment scores
-interface AfinnScores {
+interface SentimentScores {
   [key: string]: number;
 }
+
+// Create a sentiment analyzer using the Natural library
+const sentimentAnalyzer = new natural.SentimentAnalyzer("English", natural.PorterStemmer, "afinn");
+const tokenizer = new natural.WordTokenizer();
+
 export const addGraph = (key: string, value: string) => {
-  // Get the overall score for the content
   const totalScore = sentimentScore(value);
 
-  // Update the graphDot array in localStorage
   graphDot.push({ date: key, score: totalScore });
   localStorage.setItem("graphDot", JSON.stringify(graphDot));
 };
 
 export const deleteGraph = (selectedDate: string) => {
-  // Find the index of the dot with the selected date
-  const dotIndex = graphDot.findIndex(dot => dot.date === selectedDate);
+  const dotIndex = graphDot.findIndex((dot) => dot.date === selectedDate);
 
-  // If the date exists in the graphDot array, remove the dot
   if (dotIndex !== -1) {
     graphDot.splice(dotIndex, 1);
-
-    // Update the graphDot array in localStorage
-    localStorage.setItem('graphDot', JSON.stringify(graphDot));
+    localStorage.setItem("graphDot", JSON.stringify(graphDot));
   }
 };
 
 const sentimentScore = (value: string) => {
-  // Define the input text and split it into tokens
   const content = value.toLowerCase();
-  const tokens = content.split(" ");
+  const tokens = tokenizer.tokenize(content);
 
   let totalScore = 0;
-  for (const token of tokens) {
-    if (token in afinn) {
-      const scores = afinn as unknown as AfinnScores;
-      totalScore += scores[token];
+  if (tokens !== null) {
+    for (const token of tokens) {
+      const stemmedToken = natural.PorterStemmer.stem(token);
+      const score = sentimentAnalyzer.getSentiment([stemmedToken]);
+      totalScore += score;
     }
   }
 
