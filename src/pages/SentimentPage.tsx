cd7396a6588de76198c273d3a56b7399ccb5a871
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-02 18:32:20 +0700

    graph UI

--- src/pages/SentimentPage.tsx
+++ src/pages/SentimentPage.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import React from "react";
+import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import graphDot from "./Functionality/InitializeDot";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const dates = graphDot.map((obj) => obj.date);
+const scores = graphDot.map((obj) => obj.score);
+
+const data = {
+  labels: dates,
+  datasets: [
+    {
+      label: "mood change within a month",
+      data: scores,
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+    },
+  ],
+};
 
 const SentimentPage = () => (
   <>
@@ -11,13 +50,12 @@ const SentimentPage = () => (
     <IonContent scrollY={false}>
       <div
         style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%',
-        }}
-      >
-        Sentiment content
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+        }}>
+        <Line data={data} />
       </div>
     </IonContent>
   </>
