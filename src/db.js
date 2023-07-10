7e776a6bba36977bb2c35f246c75b02d56b8448e
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-10 10:29:57 +0700

    files temporary

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
-  host: 'your_database_host',
-  user: 'your_database_user',
-  password: 'your_database_password',
-  database: 'your_database_name',
-});
+// const connection = mysql.createPool({
+//   host: 'your_database_host',
+//   user: 'your_database_user',
+//   password: 'your_database_password',
+//   database: 'your_database_name',
+// });
 
-module.exports = connection;
+// module.exports = connection;
