ddb9ad4cf5b9cf0869ad1e64a196680bd7292bb5
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-15 13:58:07 +0700

    Entry) removed unnecessary codes

--- src/data/Entry/Entry.tsx
+++ src/data/Entry/Entry.tsx
@@ -11,17 +11,4 @@ type Entry = {
   id: number | undefined;
 };
 
-interface EntryService {
-  // Get Entry
-  getAllEntries(): Entry[];
-  getEntryByTitle(title: string): Entry | undefined;
-  getEntryByDate(date: string): Entry | undefined;
-  getEntryByCategory(category: string): Entry | undefined; // Updated parameter type
-  // Add Entry
-  addEntryByCategory(category: string, entry: Entry): void; // Updated parameter type
-  addEntryByDate(date: string, entry: Entry): void;
-  // Etc.
-  updateEntry(category: string, id: string, entry: Entry): boolean; // Updated parameter type
-  deleteEntry(category: string, id: string): boolean; // Updated parameter type
-  searchEntries(category: string, keyword: string): Entry[]; // Updated parameter type
-}
+
