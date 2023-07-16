cdeca6ab767678ef28c292d5ad3947232756f770
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-16 19:21:32 +0700

    VIewDatePage without separation from ViewList

--- src/pages/ViewList/ViewDatePage.tsx
+++ src/pages/ViewList/ViewDatePage.tsx
@@ -1,47 +1,70 @@
 import { useState, useRef, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import {
   IonContent,
   IonSearchbar,
   IonIcon,
   IonButton,
   IonButtons,
+  IonLabel,
+  IonList,
+  IonItem,
 } from "@ionic/react";
-import ViewList from "./ViewListSub/ViewList";
+//import ViewList from "./ViewListSub/ViewList";
 import useFetchEntriesData from "./ViewListSub/fetchEntriesData";
-import { closeCircleOutline } from "ionicons/icons";
 import { EntriesData } from "../../GetPutData";
+import { closeCircleOutline } from "ionicons/icons";
 
 const ViewDatePage = () => {
   const location = useLocation<{ selectedDate?: string }>();
   const selectedDate = location?.state?.selectedDate || "";
   const entriesData = useFetchEntriesData();
 
-  const filteredEntries = entriesData.filter(
-    (entry) => entry.date === selectedDate
-  );
-
   const [editMode, setEditMode] = useState(false);
   const [entries, setEntries] = useState<Entry[]>([]);
   const [selectedEntry, setSelectedEntry] = useState<Entry | null>(null);
-
+  const page = useRef(undefined);
+  const [presentingElement, setPresentingElement] = useState<
+    HTMLElement | undefined
+  >(undefined);
+  const history = useHistory();
 
   useEffect(() => {
-    //setPresentingElement(page.current);
+    setPresentingElement(page.current);
     // get data from backend
-    const entriesData = new EntriesData();
-    entriesData
-      .getEntriesData()
-      .then((data) => {
-        setEntries(data);
-      })
-      .catch((error) => {
+    const fetchData = async () => {
+      try {
+        const data = await entriesData;
+        setEntries(data.filter((entry) => entry.date === selectedDate));
+      } catch (error) {
         console.error("Error fetching entries:", error);
-      });
-  }, []);
+      }
+    };
+    fetchData();
+  }, [entriesData, selectedDate]); // Update filteredEntries when entriesData or selectedDate changes
+
+
+  const handleDeleteEntry = (index: number, entry: Entry) => {
+    console.log("delete clicked");
+    console.log("entry before updating", entry);
+    const updatedData = entries.filter((_, i) => i !== index);
+    console.log("entry after updating", updatedData);
+    setEntries(updatedData);
+
+    const entriesData = new EntriesData();
+    entriesData.deleteEntriesData(entry);
+  };
+
+  const handleEntryClick = (selectedEntry: Entry) => {
+    console.log("entry clicked");
+    setSelectedEntry(selectedEntry);
+    history.push("/view", { entryData: selectedEntry });
+  };
 
   const handleEditClick = () => {
-    setEditMode(true); 
+    // Set the selected entry to the first entry in the list
+    setSelectedEntry(entries[0]);
+    setEditMode(true);
   };
 
   return (
@@ -82,12 +105,68 @@ const ViewDatePage = () => {
         </div>
 
         <IonSearchbar showClearButton="focus"></IonSearchbar>
-        <ViewList
-          editMode={editMode}
-          setEditMode={setEditMode}
-          entries={filteredEntries}
-          selectedDate={selectedDate}
-        />
+        <IonList>
+          {entries.map((entry, index) => (
+            <IonItem
+              key={entry.id}
+              style={{ padding: "7px", fontSize: "18px" }}>
+              {editMode && (
+                <IonIcon
+                  icon={closeCircleOutline}
+                  style={{
+                    fontSize: "22px",
+                    marginRight: "10px",
+                  }}
+                  onClick={() => {
+                    setSelectedEntry(entry);
+                    handleDeleteEntry(index, entry);
+                  }}
+                />
+              )}
+              <IonLabel onClick={() => handleEntryClick(entry)}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "100%",
+                  }}>
+                  <div style={{ display: "flex", flexDirection: "row" }}>
+                    <div
+                      style={{
+                        fontSize: "21px",
+                        fontWeight: "bold",
+                        marginRight: "auto",
+                      }}>
+                      {entry.title}
+                    </div>
+                    <div
+                      style={{
+                        fontSize: "14px",
+                        color: "rgb(165, 165, 165)",
+                        marginLeft: "auto",
+                        marginTop: "10px",
+                      }}>
+                      {entry.category}
+                    </div>
+                  </div>
+
+                  <div
+                    style={{
+                      fontSize: "16px",
+                      marginTop: "5px",
+                      marginBottom: "15px",
+                    }}>
+                    {entry.content && entry.content[0] && entry.content[0].text
+                      ? entry.content[0].text.length > 30
+                        ? `${entry.content[0].text.substring(0, 88)}...`
+                        : entry.content[0].text
+                      : ""}
+                  </div>
+                </div>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </>
   );
--- src/pages/ViewList/ViewListSub/ViewList.tsx
+++ src/pages/ViewList/ViewListSub/ViewList.tsx
@@ -3,37 +3,47 @@ import { IonItem, IonList, IonIcon, IonLabel } from "@ionic/react";
 import { EntriesData } from "../../../GetPutData";
 import { closeCircleOutline, informationCircleOutline } from "ionicons/icons";
 
-
 interface ViewListProps {
   editMode: boolean;
-  setEditMode: React.Dispatch<React.SetStateAction<boolean>>;
   entries: Entry[];
+  setEntries: React.Dispatch<React.SetStateAction<Entry[]>>;
+  setSelectedEntry: (entry: Entry) => void;
   selectedDate: string;
 }
 
 const ViewList: React.FC<ViewListProps> = ({
   editMode,
-  setEditMode,
   entries,
+  setEntries,
+  setSelectedEntry,
   selectedDate,
 }) => {
   const history = useHistory();
 
-  const handleDeleteEntry = (index: number) => {
+  const filteredEntries = entries.filter(
+    (entry) => entry.date === selectedDate
+  );
+
+  const handleDeleteEntry = (index: number, entry: Entry) => {
+    console.log("delete clicked");
+    console.log("entry before updating", entry);
     const updatedData = entries.filter((_, i) => i !== index);
+    console.log("entry after updating", updatedData);
+    setEntries(updatedData);
+
     const entriesData = new EntriesData();
-    // entriesData.putEntriesData(updatedData);
-    // setEntries(updatedData);
+    entriesData.deleteEntriesData(entry);
   };
 
   const handleEntryClick = (selectedEntry: Entry) => {
-      history.push("/view", { entryData: selectedEntry });
+    console.log("entry clicked");
+    setSelectedEntry(selectedEntry);
+    history.push("/view", { entryData: selectedEntry });
   };
 
   return (
     <IonList>
-
-      {entries.map((entry, index) => (
+      {filteredEntries.map((entry, index) => (
         <IonItem key={entry.id} style={{ padding: "7px", fontSize: "18px" }}>
           {editMode && (
             <IonIcon
@@ -42,51 +52,53 @@ const ViewList: React.FC<ViewListProps> = ({
                 fontSize: "22px",
                 marginRight: "10px",
               }}
-              onClick={() => handleDeleteEntry(index)}
+              onClick={() => {
+                setSelectedEntry(entry);
+                handleDeleteEntry(index, entry);
+              }}
             />
           )}
           <IonLabel onClick={() => handleEntryClick(entry)}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-            }}>
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <div
-                style={{
-                  fontSize: "21px",
-                  fontWeight: "bold",
-                  marginRight: "auto",
-                }}>
-                {entry.title}
-              </div>
-              <div
-                style={{
-                  fontSize: "14px",
-                  color: "rgb(165, 165, 165)",
-                  marginLeft: "auto",
-                  marginTop: "10px",
-                }}>
-                {entry.category}
-              </div>
-            </div>
-
             <div
               style={{
-                fontSize: "16px",
-                marginTop: "5px",
-                marginBottom: "15px",
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
               }}>
-              {entry.content && entry.content[0] && entry.content[0].text
-                ? entry.content[0].text.length > 30
-                  ? `${entry.content[0].text.substring(0, 88)}...`
-                  : entry.content[0].text
-                : ""}
-            </div>
-          </div>
-          </IonLabel>
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <div
+                  style={{
+                    fontSize: "21px",
+                    fontWeight: "bold",
+                    marginRight: "auto",
+                  }}>
+                  {entry.title}
+                </div>
+                <div
+                  style={{
+                    fontSize: "14px",
+                    color: "rgb(165, 165, 165)",
+                    marginLeft: "auto",
+                    marginTop: "10px",
+                  }}>
+                  {entry.category}
+                </div>
+              </div>
 
+              <div
+                style={{
+                  fontSize: "16px",
+                  marginTop: "5px",
+                  marginBottom: "15px",
+                }}>
+                {entry.content && entry.content[0] && entry.content[0].text
+                  ? entry.content[0].text.length > 30
+                    ? `${entry.content[0].text.substring(0, 88)}...`
+                    : entry.content[0].text
+                  : ""}
+              </div>
+            </div>
+          </IonLabel>
         </IonItem>
       ))}
     </IonList>
