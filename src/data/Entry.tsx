a8d055d08c92530fa1148eb3c9ca3e125acc7373
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-20 01:38:29 +0700

    initial setup

--- src/data/Entry.tsx
+++ src/data/Entry.tsx
@@ -1,30 +1,12 @@
 type Content = {
-    type: "text" | "image" | "video" | "audio" | "location" | "sticker";
-  };
-  
-  type Entry = {
-    content: Content[];
-    date: Date;
-    title: string;
-  };
-  
-  type Category = {
-    category: string;
-    entries: Entry[];
-  };
-  
-  interface EntryService {
-    //Get Entry
-    getAllEntries(): Entry[];
-    getEntryByTitle(title: string): Entry | undefined;
-    getEntryByDate(date: Date): Entry | undefined;
-    getEntryByCategory(category: Category): Entry | undefined;
-    //Add Entry
-    addEntryByCategory(category: Category, entry: Entry): void;
-    addEntryByDate(date: Date, entry: Entry): void;
-    // Etc.
-    updateEntry(category: Category, id: string, entry: Entry): boolean;
-    deleteEntry(category: Category, id: string): boolean;
-    searchEntries(category: Category, keyword: string): Entry[];
-  }
-  
+  type: "text" | "image" | "video" | "audio" | "location" | "sticker";
+  text?: string;
+};
+
+type Entry = {
+  id: number; // The unique integer ID set from the backend
+  content: Content[];
+  date: string;
+  title: string;
+  category: string;
+};
