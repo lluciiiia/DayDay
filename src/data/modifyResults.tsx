7286c64041b48f7795a9a8b281e954a43b356716
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-20 14:27:56 +0700

    modifyResults

---
+++ src/data/modifyResults.tsx
@@ -0,0 +1,37 @@
+import { TimeBasedResultData } from "./ResultService/TimeBasedResult";
+import { SentimentResultData } from "./ResultService/sentimentResult";
+import { ActivityGoalResultData } from "./ResultService/ActivityGoalResult";
+import { LocationResultData } from "./ResultService/locationResult";
+import { EmotionalMappingResultData } from "./ResultService/MappingResult";
+
+export class modifyResults {
+  private timeBasedResult = new TimeBasedResultData();
+  private sentimentResult = new SentimentResultData();
+  private activityGoalResult = new ActivityGoalResultData();
+  private locationResult = new LocationResultData();
+  private emotionalMappingResult = new EmotionalMappingResultData();
+
+  async addResultData(entryId: number, analysisResult: any) {
+    this.timeBasedResult.addResult(entryId, analysisResult);
+    this.sentimentResult.addResult(entryId, analysisResult);
+    this.activityGoalResult.addResult(entryId, analysisResult);
+    this.locationResult.addResult(entryId, analysisResult);
+    this.emotionalMappingResult.addResult(entryId, analysisResult);
+  }
+
+  async editResultData(entryId: number, updatedResult: any) {
+    this.timeBasedResult.editResult(entryId, updatedResult);
+    this.sentimentResult.editResult(entryId, updatedResult);
+    this.activityGoalResult.editResult(entryId, updatedResult);
+    this.locationResult.editResult(entryId, updatedResult);
+    this.emotionalMappingResult.editResult(entryId, updatedResult);
+  }
+
+  async deleteResultData(entryId: number) {
+    this.timeBasedResult.deleteResult(entryId);
+    this.sentimentResult.deleteResult(entryId);
+    this.activityGoalResult.deleteResult(entryId);
+    this.locationResult.deleteResult(entryId);
+    this.emotionalMappingResult.deleteResult(entryId);
+  }
+}
