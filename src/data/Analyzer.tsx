3b218e2ea528de68dc9ba653cf28907402832bf4
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-20 14:08:34 +0700

    Analysis structure

--- src/data/Analyzer.tsx
+++ src/data/Analyzer.tsx
@@ -3,11 +3,32 @@ import { Entry, LocationData } from "./interfaces";
 export interface EntryAnalysis {
   analyzeSentiment(entry: Entry): Promise<string>;
   analyzeLocation(entry: Entry): Promise<LocationData>;
+  analyzeEmotionalMapping(entry: Entry): Promise<void>;
+  analyzeActivityAndGoal(entry: Entry): Promise<void>;
+  analyzeTimeBased(entry: Entry): Promise<void>;
 }
 
-//the methods that the plotter class should implement to handle the visualization of different types of analysis results
-export interface AnalysisPlot {
-  plotSentiment(sentiment: string): void;
-  plotLocation(location: LocationData): void;
-  // Add other plot methods for different analysis types if needed in the future
+export class EntryAnalyzer implements EntryAnalysis {
+  async analyzeSentiment(entry: Entry): Promise<string> {
+    // Implementation for sentiment analysis
+    const sentimentResult = /* Perform sentiment analysis */ "";
+    return sentimentResult;
+  }
+
+  async analyzeLocation(entry: Entry): Promise<LocationData> {
+    // Implementation for location-based analysis
+    return { latitude: 0, longitude: 0 };
+  }
+
+  async analyzeEmotionalMapping(entry: Entry): Promise<void> {
+    // Implementation for emotional mapping analysis
+  }
+
+  async analyzeActivityAndGoal(entry: Entry): Promise<void> {
+    // Implementation for activity and goal analysis
+  }
+
+  async analyzeTimeBased(entry: Entry): Promise<void> {
+    // Implementation for time-based analysis
+  }
 }
