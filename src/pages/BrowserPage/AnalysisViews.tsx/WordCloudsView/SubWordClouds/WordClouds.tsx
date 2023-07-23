e8d8896e748db18f1e14a8fe1ce257edae4168d3
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-07-23 14:12:57 +0700

    word clouds

--- src/pages/BrowserPage/AnalysisViews.tsx/WordCloudsView/SubWordClouds/WordClouds.tsx
+++ src/pages/BrowserPage/AnalysisViews.tsx/WordCloudsView/SubWordClouds/WordClouds.tsx
@@ -1,16 +1,48 @@
 import { IonList, IonLabel, IonItem, IonContent } from "@ionic/react";
+import wordCloud from "wordcloud";
+import { useRef, useEffect } from "react";
 
 interface WordCloudsProps {
   topWords: [string, number][];
 }
 
 export const WordClouds: React.FC<WordCloudsProps> = ({ topWords }) => {
+  const cloudRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (cloudRef.current) {
+      const canvas = cloudRef.current;
+      const ctx = canvas.getContext("2d");
+
+      if (ctx) {
+        wordCloud(canvas, {
+          list: topWords,
+          fontFamily: "Arial",
+          gridSize: 10,
+          weightFactor: 25,
+          backgroundColor: "rgba(0,0,0)",
+        });
+
+        // Optionally, you can add event listeners or further customization to the word cloud canvas here.
+      }
+    }
+
+    return () => {
+      // Clean up the word cloud canvas on unmount by clearing its content.
+      if (cloudRef.current) {
+        const canvas = cloudRef.current;
+        const ctx = canvas.getContext("2d");
+        if (ctx) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+      }
+    };
+  }, [topWords]);
+
   return (
-    <>
-      <IonContent
-        scrollY={true}
-        style={{ height: "calc(100% - 75px)" }}></IonContent>
-    </>
+    <IonContent scrollY={false} style={{ height: "calc(100% - 75px)" }}>
+      <canvas ref={cloudRef} width="380" height="650" />
+    </IonContent>
   );
 };
 
