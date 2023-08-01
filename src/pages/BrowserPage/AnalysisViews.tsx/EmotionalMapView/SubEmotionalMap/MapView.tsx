8e37ea420fa36c1358c4f8eb1659c56cec624881
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-08-01 20:33:40 +0700

    SVG codes separation

--- src/pages/BrowserPage/AnalysisViews.tsx/EmotionalMapView/SubEmotionalMap/MapView.tsx
+++ src/pages/BrowserPage/AnalysisViews.tsx/EmotionalMapView/SubEmotionalMap/MapView.tsx
@@ -34,52 +34,12 @@ export const MapView = () => {
     if (!apiKey) {
       fetchGooglePlacesApiKey();
     }
-  }, [apiKey]); 
+  }, [apiKey]);
 
   const filteredEntries = FilteredEntries();
   if (filteredEntries === null) {
     return [];
   }
-
-  console.log(
-    "filteredEntries.lat: ",
-    filteredEntries.map((filteredEntry) => filteredEntry.lat)
-  );
-  console.log(
-    "filteredEntries.lng: ",
-    filteredEntries.map((filteredEntry) => filteredEntry.lng)
-  );
-  console.log(
-    "filteredEntries.placeId: ",
-    filteredEntries.map((filteredEntry) => filteredEntry.placeId)
-  );
-  console.log(
-    "filteredEntries.placeName: ",
-    filteredEntries.map((filteredEntry) => filteredEntry.placeName)
-  );
-
-//   const happyIcon = `
-//   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-//     <path d="M12 1a11 11 0 1 0 11 11A11.013 11.013 0 0 0 12 1zm0 20a9 9 0 1 1 9-9 9.01 9.01 0 0 1-9 9zM8 11V9a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0zm8-2v2a1 1 0 0 1-2 0V9a1 1 0 0 1 2 0zm-8 5h8a4 4 0 0 1-8 0z"/>
-//   </svg>
-// `;
-
-//   const sadIcon = `
-//   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-//     <path d="M8 11V9a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0zm7 1a1 1 0 0 0 1-1V9a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1zm-3 2a6.036 6.036 0 0 0-4.775 2.368 1 1 0 1 0 1.55 1.264 4 4 0 0 1 6.45 0 1 1 0 0 0 1.55-1.264A6.036 6.036 0 0 0 12 14zm11-2A11 11 0 1 1 12 1a11.013 11.013 0 0 1 11 11zm-2 0a9 9 0 1 0-9 9 9.01 9.01 0 0 0 9-9z"/>
-//   </svg>
-// `;
-
-//   const middleIcon = `
-//   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-//     <path d="M10 9v2a1 1 0 0 1-2 0V9a1 1 0 0 1 2 0zm5-1a1 1 0 0 0-1 1v2a1 1 0 0 0 2 0V9a1 1 0 0 0-1-1zm8 4A11 11 0 1 1 12 1a11.013 11.013 0 0 1 11 11zm-2 0a9 9 0 1 0-9 9 9.01 9.01 0 0 0 9-9z"/>
-//   </svg>
-// `;
-
-//   // SVG code for icons
-//   const HappyDataUrl = `data:image/svg+xml;base64,${btoa(happyIcon)}`;
-//   const SadDataUrl = `data:image/svg+xml;base64,${btoa(sadIcon)}`;
-//   const MiddleDataUrl = `data:image/svg+xml;base64,${btoa(middleIcon)}`;
 
   const renderMarkers = () => {
     return filteredEntries.map((filteredEntry) => (
@@ -119,11 +79,9 @@ export const MapView = () => {
     ));
   };
 
-  console.log("apiKey before Load: ", apiKey);
-
-  return apiKey ? ( 
+  return apiKey ? (
     <LoadScript googleMapsApiKey={apiKey} onLoad={() => setMapLoaded(true)}>
-      {mapLoaded && ( 
+      {mapLoaded && (
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           center={center}
